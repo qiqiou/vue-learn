df9535a1f21404aa889fe45dfb6df2efcb59003d
qiqiou(欧琪) <qiqiou@futunn.com>
2019-12-20 14:35:31 +0800

    init

---
+++ src/components/Created.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      message: 'hello Vue!'
+    }
+  },
+  // 创建之前
+  beforeCreate(){
+    console.log('beforeCreate 即将创建');
+    console.log("%c%s", "color:red","el     : " + this.$el);
+    console.log("%c%s", "color:red","method : " + this.getMessage)
+    console.log("%c%s", "color:red","data   : " + JSON.stringify(this.$data));
+  },
+  // 创建完毕
+  created(){
+    console.log("%c%s", "color:#00965e",'created 创建完毕');
+    console.log("%c%s", "color:red" , "el     : " + this.$el);
+    console.log("%c%s", "color:red","method: " + this.getMessage)
+    console.log("%c%s", "color:red","data   : " + JSON.stringify(this.$data));
+  },
+  methods: {
+    getMessage() {
+      return this.message;
+    }
+  }
+}
+</script>
---
+++ src/components/child1.vue
@@ -0,0 +1,52 @@
+<template>
+  <div style="background-color: #eeeeee;padding:5px 10px">
+    <div>child1:</div>
+    <div>
+      {{child}}
+    </div>
+    <input type="text" v-model="child" style="border: 1px solid #aaaaaa"/>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+        type: [String,Number],
+        default: 'qiqiou'
+    }
+  },
+  data(){
+    return {
+      child: 999
+    }
+  },
+    // 创建之前
+  beforeCreate(){
+    console.log('   child1 beforeCreate 即将创建');
+  },
+  // 创建完毕
+  created(){
+    console.log('   child1 created 创建完毕');
+  },
+  // 挂载前，#el挂载到我们指定的节点
+  beforeMount(){
+    console.log('   child1 beforeMount 即将挂载');
+  },
+  // 挂载完毕
+  mounted(){
+    console.log('   child1 mounted 挂载完毕');
+  },
+  beforeUpdate(){
+    console.log('   child1 beforeUpdate 更新前');
+  },
+  updated(){
+    console.log('   child1 updated 更新完毕');
+  },
+  beforeDestroy(){
+    console.log('   child1 beforeDestroy 摧毁前');
+  },
+  destroyed(){
+    console.log('   child1 destroyed 摧毁完毕');
+  }
+}
+</script>
